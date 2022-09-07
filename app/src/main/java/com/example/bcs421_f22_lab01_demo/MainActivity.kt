25b96d3e292abc630decb20897abc391b80ddfc7
LuiscLopez <luisclopez1999@gmail.com>
2022-09-07 13:08:19 -0400

    Initial commit

---
+++ app/src/main/java/com/example/bcs421_f22_lab01_demo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.bcs421_f22_lab01_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //ToDo 01: print name here
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //ToDo 02: print name here
+    }
+}
